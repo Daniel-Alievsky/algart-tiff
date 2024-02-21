0684022a0ec1a327e1d2b94caa55be4b6f7724e1
Daniel Alievsky <daniel.alievsky@gmail.com>
2024-02-21 16:35:04 +0200

    Help classes, imported from SCIFIO: reformatting code

--- src/main/java/net/algart/matrices/tiff/awt/SignedShortBuffer.java
+++ src/main/java/net/algart/matrices/tiff/awt/SignedShortBuffer.java
@@ -35,92 +35,88 @@ import java.awt.image.DataBufferShort;
  * DataBuffers with type DataBuffer.TYPE_SHORT.
  */
 public class SignedShortBuffer extends DataBuffer {
-	// (It is placed here to avoid autocorrection by IntelliJ IDEA)
-	/*
-	 * #%L
-	 * SCIFIO library for reading and converting scientific file formats.
-	 * %%
-	 * Copyright (C) 2011 - 2023 SCIFIO developers.
-	 * %%
-	 * Redistribution and use in source and binary forms, with or without
-	 * modification, are permitted provided that the following conditions are met:
-	 *
-	 * 1. Redistributions of source code must retain the above copyright notice,
-	 *    this list of conditions and the following disclaimer.
-	 * 2. Redistributions in binary form must reproduce the above copyright notice,
-	 *    this list of conditions and the following disclaimer in the documentation
-	 *    and/or other materials provided with the distribution.
-	 *
-	 * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-	 * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-	 * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-	 * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
-	 * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-	 * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-	 * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-	 * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-	 * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-	 * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-	 * POSSIBILITY OF SUCH DAMAGE.
-	 * #L%
-	 */
+    // (It is placed here to avoid autocorrection by IntelliJ IDEA)
+    /*
+     * #%L
+     * SCIFIO library for reading and converting scientific file formats.
+     * %%
+     * Copyright (C) 2011 - 2023 SCIFIO developers.
+     * %%
+     * Redistribution and use in source and binary forms, with or without
+     * modification, are permitted provided that the following conditions are met:
+     *
+     * 1. Redistributions of source code must retain the above copyright notice,
+     *    this list of conditions and the following disclaimer.
+     * 2. Redistributions in binary form must reproduce the above copyright notice,
+     *    this list of conditions and the following disclaimer in the documentation
+     *    and/or other materials provided with the distribution.
+     *
+     * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+     * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+     * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+     * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+     * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+     * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+     * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+     * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+     * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+     * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+     * POSSIBILITY OF SUCH DAMAGE.
+     * #L%
+     */
 
-	private final DataBufferShort helper;
+    private final DataBufferShort helper;
 
-	// -- Constructors --
+    // -- Constructors --
 
-	public SignedShortBuffer(final int size) {
-		super(DataBuffer.TYPE_USHORT, size);
-		helper = new DataBufferShort(size);
-	}
+    public SignedShortBuffer(int size) {
+        super(DataBuffer.TYPE_USHORT, size);
+        helper = new DataBufferShort(size);
+    }
 
-	public SignedShortBuffer(final int size, final int numbanks) {
-		super(DataBuffer.TYPE_USHORT, size, numbanks);
-		helper = new DataBufferShort(size, numbanks);
-	}
+    public SignedShortBuffer(int size, int numbanks) {
+        super(DataBuffer.TYPE_USHORT, size, numbanks);
+        helper = new DataBufferShort(size, numbanks);
+    }
 
-	public SignedShortBuffer(final short[] data, final int size) {
-		super(DataBuffer.TYPE_USHORT, size);
-		helper = new DataBufferShort(data, size);
-	}
+    public SignedShortBuffer(short[] data, int size) {
+        super(DataBuffer.TYPE_USHORT, size);
+        helper = new DataBufferShort(data, size);
+    }
 
-	public SignedShortBuffer(final short[] data, final int size,
-		final int offset)
-	{
-		super(DataBuffer.TYPE_USHORT, size, 1, offset);
-		helper = new DataBufferShort(data, size, offset);
-	}
+    public SignedShortBuffer(short[] data, int size, int offset) {
+        super(DataBuffer.TYPE_USHORT, size, 1, offset);
+        helper = new DataBufferShort(data, size, offset);
+    }
 
-	public SignedShortBuffer(final short[][] data, final int size) {
-		super(DataBuffer.TYPE_USHORT, size, data.length);
-		helper = new DataBufferShort(data, size);
-	}
+    public SignedShortBuffer(short[][] data, int size) {
+        super(DataBuffer.TYPE_USHORT, size, data.length);
+        helper = new DataBufferShort(data, size);
+    }
 
-	public SignedShortBuffer(final short[][] data, final int size,
-		final int[] offsets)
-	{
-		super(DataBuffer.TYPE_USHORT, size, data.length, offsets);
-		helper = new DataBufferShort(data, size, offsets);
-	}
+    public SignedShortBuffer(short[][] data, int size, int[] offsets) {
+        super(DataBuffer.TYPE_USHORT, size, data.length, offsets);
+        helper = new DataBufferShort(data, size, offsets);
+    }
 
-	// -- DataBuffer API methods --
+    // -- DataBuffer API methods --
 
-	public short[] getData() {
-		return helper.getData();
-	}
+    public short[] getData() {
+        return helper.getData();
+    }
 
-	public short[] getData(final int bank) {
-		return helper.getData(bank);
-	}
+    public short[] getData(int bank) {
+        return helper.getData(bank);
+    }
 
-	@Override
-	public int getElem(final int bank, final int i) {
-		return helper.getElem(bank, i);
-	}
+    @Override
+    public int getElem(int bank, int i) {
+        return helper.getElem(bank, i);
+    }
 
-	@Override
-	public void setElem(final int bank, final int i, final int val) {
-		helper.setElem(bank, i, val);
-	}
+    @Override
+    public void setElem(int bank, int i, int val) {
+        helper.setElem(bank, i, val);
+    }
 
 }
--- src/main/java/net/algart/matrices/tiff/awt/UnsignedIntBuffer.java
+++ src/main/java/net/algart/matrices/tiff/awt/UnsignedIntBuffer.java
@@ -32,119 +32,123 @@ import java.awt.image.DataBuffer;
  * @author Melissa Linkert
  */
 public class UnsignedIntBuffer extends DataBuffer {
-	// (It is placed here to avoid autocorrection by IntelliJ IDEA)
-	/*
-	 * #%L
-	 * SCIFIO library for reading and converting scientific file formats.
-	 * %%
-	 * Copyright (C) 2011 - 2023 SCIFIO developers.
-	 * %%
-	 * Redistribution and use in source and binary forms, with or without
-	 * modification, are permitted provided that the following conditions are met:
-	 *
-	 * 1. Redistributions of source code must retain the above copyright notice,
-	 *    this list of conditions and the following disclaimer.
-	 * 2. Redistributions in binary form must reproduce the above copyright notice,
-	 *    this list of conditions and the following disclaimer in the documentation
-	 *    and/or other materials provided with the distribution.
-	 *
-	 * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-	 * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-	 * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-	 * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
-	 * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-	 * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-	 * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-	 * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-	 * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-	 * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-	 * POSSIBILITY OF SUCH DAMAGE.
-	 * #L%
-	 */
+    // (It is placed here to avoid autocorrection by IntelliJ IDEA)
+    /*
+     * #%L
+     * SCIFIO library for reading and converting scientific file formats.
+     * %%
+     * Copyright (C) 2011 - 2023 SCIFIO developers.
+     * %%
+     * Redistribution and use in source and binary forms, with or without
+     * modification, are permitted provided that the following conditions are met:
+     *
+     * 1. Redistributions of source code must retain the above copyright notice,
+     *    this list of conditions and the following disclaimer.
+     * 2. Redistributions in binary form must reproduce the above copyright notice,
+     *    this list of conditions and the following disclaimer in the documentation
+     *    and/or other materials provided with the distribution.
+     *
+     * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+     * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+     * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+     * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+     * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+     * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+     * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+     * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+     * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+     * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+     * POSSIBILITY OF SUCH DAMAGE.
+     * #L%
+     */
 
-	private final int[][] bankData;
+    private final int[][] bankData;
 
-	/** Construct a new buffer of unsigned ints using the given int array. */
-	public UnsignedIntBuffer(final int[] dataArray, final int size) {
-		super(DataBuffer.TYPE_INT, size);
-		bankData = new int[1][];
-		bankData[0] = dataArray;
-	}
+    /**
+     * Construct a new buffer of unsigned ints using the given int array.
+     */
+    public UnsignedIntBuffer(int[] dataArray, int size) {
+        super(DataBuffer.TYPE_INT, size);
+        bankData = new int[1][];
+        bankData[0] = dataArray;
+    }
 
-	/** Construct a new buffer of unsigned ints using the given 2D int array. */
-	public UnsignedIntBuffer(final int[][] dataArray, final int size) {
+    /**
+     * Construct a new buffer of unsigned ints using the given 2D int array.
+     */
+    public UnsignedIntBuffer(int[][] dataArray, int size) {
 //		super(DataBuffer.TYPE_INT, size); // SCIFIO BUG!
-		super(DataBuffer.TYPE_INT, size, dataArray.length);
-		bankData = dataArray;
-	}
+        super(DataBuffer.TYPE_INT, size, dataArray.length);
+        bankData = dataArray;
+    }
 
-	public int[] getData() {
-		return bankData[0];
-	}
+    public int[] getData() {
+        return bankData[0];
+    }
 
-	public int[] getData(final int bank) {
-		return bankData[bank];
-	}
+    public int[] getData(int bank) {
+        return bankData[bank];
+    }
 
-	@Override
-	public int getElem(final int i) {
-		return getElem(0, i);
-	}
+    @Override
+    public int getElem(int i) {
+        return getElem(0, i);
+    }
 
-	@Override
-	public int getElem(final int bank, final int i) {
-		final int value = bankData[bank][i + getOffsets()[bank]];
-		return (int) (value & 0xffffffffL);
-	}
+    @Override
+    public int getElem(int bank, int i) {
+        final int value = bankData[bank][i + getOffsets()[bank]];
+        return (int) (value & 0xffffffffL);
+    }
 
-	@Override
-	public float getElemFloat(final int i) {
-		return getElemFloat(0, i);
-	}
+    @Override
+    public float getElemFloat(int i) {
+        return getElemFloat(0, i);
+    }
 
-	@Override
-	public float getElemFloat(final int bank, final int i) {
-		return (getElem(bank, i) & 0xffffffffL);
-	}
+    @Override
+    public float getElemFloat(int bank, int i) {
+        return (getElem(bank, i) & 0xffffffffL);
+    }
 
-	@Override
-	public double getElemDouble(final int i) {
-		return getElemDouble(0, i);
-	}
+    @Override
+    public double getElemDouble(int i) {
+        return getElemDouble(0, i);
+    }
 
-	@Override
-	public double getElemDouble(final int bank, final int i) {
-		return (getElem(bank, i) & 0xffffffffL);
-	}
+    @Override
+    public double getElemDouble(int bank, int i) {
+        return (getElem(bank, i) & 0xffffffffL);
+    }
 
-	@Override
-	public void setElem(final int i, final int val) {
-		setElem(0, i, val);
-	}
+    @Override
+    public void setElem(int i, int val) {
+        setElem(0, i, val);
+    }
 
-	@Override
-	public void setElem(final int bank, final int i, final int val) {
-		bankData[bank][i + getOffsets()[bank]] = val;
-	}
+    @Override
+    public void setElem(int bank, int i, int val) {
+        bankData[bank][i + getOffsets()[bank]] = val;
+    }
 
-	@Override
-	public void setElemFloat(final int i, final float val) {
-		setElemFloat(0, i, val);
-	}
+    @Override
+    public void setElemFloat(int i, float val) {
+        setElemFloat(0, i, val);
+    }
 
-	@Override
-	public void setElemFloat(final int bank, final int i, final float val) {
-		bankData[bank][i + getOffsets()[bank]] = (int) val;
-	}
+    @Override
+    public void setElemFloat(int bank, int i, float val) {
+        bankData[bank][i + getOffsets()[bank]] = (int) val;
+    }
 
-	@Override
-	public void setElemDouble(final int i, final double val) {
-		setElemDouble(0, i, val);
-	}
+    @Override
+    public void setElemDouble(int i, double val) {
+        setElemDouble(0, i, val);
+    }
 
-	@Override
-	public void setElemDouble(final int bank, final int i, final double val) {
-		bankData[bank][i + getOffsets()[bank]] = (int) val;
-	}
+    @Override
+    public void setElemDouble(int bank, int i, double val) {
+        bankData[bank][i + getOffsets()[bank]] = (int) val;
+    }
 
 }
