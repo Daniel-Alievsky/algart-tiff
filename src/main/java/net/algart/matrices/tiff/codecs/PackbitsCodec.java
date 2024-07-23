9713814c67c50ce034776ee43620ba46277fc01c
Daniel Alievsky <daniel.alievsky@gmail.com>
2024-07-23 18:55:38 +0300

    PackBits: removing previous authors (it is fully rewritten)

--- src/main/java/net/algart/matrices/tiff/codecs/PackbitsCodec.java
+++ src/main/java/net/algart/matrices/tiff/codecs/PackbitsCodec.java
@@ -36,42 +36,7 @@ import java.util.Arrays;
 import java.util.Objects;
 import java.util.Random;
 
-/**
- * This class implements packbits decompression. Compression is not yet
- * implemented.
- *
- * @author Melissa Linkert
- */
 public class PackbitsCodec implements TiffCodec {
-    // (It is placed here to avoid autocorrection by IntelliJ IDEA)
-    /*
-     * #%L
-     * SCIFIO library for reading and converting scientific file formats.
-     * %%
-     * Copyright (C) 2011 - 2023 SCIFIO developers.
-     * %%
-     * Redistribution and use in source and binary forms, with or without
-     * modification, are permitted provided that the following conditions are met:
-     *
-     * 1. Redistributions of source code must retain the above copyright notice,
-     *    this list of conditions and the following disclaimer.
-     * 2. Redistributions in binary form must reproduce the above copyright notice,
-     *    this list of conditions and the following disclaimer in the documentation
-     *    and/or other materials provided with the distribution.
-     *
-     * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-     * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-     * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-     * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
-     * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-     * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-     * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-     * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-     * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-     * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-     * POSSIBILITY OF SUCH DAMAGE.
-     * #L%
-     */
     @Override
     public byte[] compress(final byte[] data, final Options options) throws TiffException {
         Objects.requireNonNull(data, "Null data");
@@ -88,7 +53,6 @@ public class PackbitsCodec implements TiffCodec {
      * The Options parameter should have the following fields set:
      * {@link Options#getMaxSizeInBytes()}  maxBytes}
      */
-
     @Override
     public byte[] decompress(byte[] data, Options options) throws TiffException {
         Objects.requireNonNull(data, "Null data");
@@ -97,7 +61,6 @@ public class PackbitsCodec implements TiffCodec {
         unpackBytes(result, data, data.length);
         return result;
     }
-
 
     private static byte[] encodeImage(
             byte[] bytes,
