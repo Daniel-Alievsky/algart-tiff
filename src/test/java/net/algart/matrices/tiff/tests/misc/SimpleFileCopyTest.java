4b9a1c0c8e25a483944afbc5355d33fbd4604c82
Daniel Alievsky <daniel.alievsky@gmail.com>
2025-05-19 15:50:12 +0300

    Adding SimpleFileCopyTest

---
+++ src/test/java/net/algart/matrices/tiff/tests/misc/SimpleFileCopyTest.java
@@ -0,0 +1,48 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2023-2025 Daniel Alievsky, AlgART Laboratory (http://algart.net)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package net.algart.matrices.tiff.tests.misc;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+
+public class SimpleFileCopyTest {
+    public static void main(String[] args) throws IOException {
+        if (args.length < 2) {
+            System.out.println("Usage:");
+            System.out.println("    " + SimpleFileCopyTest.class.getName() + " source.file target.file");
+            return;
+        }
+        final Path file1 = Path.of(args[0]);
+        final Path file2 = Path.of(args[1]);
+        long length = Files.size(file1);
+        long t1 = System.nanoTime();
+        Files.copy(file1, file2, StandardCopyOption.REPLACE_EXISTING);
+        long t2 = System.nanoTime();
+        System.out.printf("Copied %d bytes from %s to %s in %.3f sec, %.3f MB/sec%n",
+                length, file1, file2, (t2 - t1) * 1e-9, length / 1048576.0 / ((t2 - t1) * 1e-9));
+    }
+}
