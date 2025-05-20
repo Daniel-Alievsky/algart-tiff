279b89550035871c3f088eaf0427a696b10b0ed1
Daniel Alievsky <daniel.alievsky@gmail.com>
2025-05-20 16:18:49 +0300

    Better DeflateCodecTest

--- src/test/java/net/algart/matrices/tiff/tests/codecs/DeflateCodecTest.java
+++ src/test/java/net/algart/matrices/tiff/tests/codecs/DeflateCodecTest.java
@@ -36,36 +36,41 @@ public class DeflateCodecTest {
     public static void main(String[] args) throws IOException {
         if (args.length < 2) {
             System.out.println("Usage:");
-            System.out.printf("    %s source_file packed_file [level]",
+            System.out.printf("    %s source_file packed_file [level] [number_of_tests]%n",
                     DeflateCodecTest.class.getName());
             return;
         }
         final Path sourceFile = Path.of(args[0]);
         final Path packedFile = Path.of(args[1]);
-        final Double level = args.length > 2 ? Double.parseDouble(args[2]) : null;
+        final Double level = args.length > 2 && !"null".equals(args[2]) ? Double.parseDouble(args[2]) : null;
+        final int numberOfTests = args.length > 3 ? Integer.parseInt(args[3]) : 1;
         DeflateCodec codec = new DeflateCodec();
         TiffCodec.Options options = new TiffCodec.Options();
         if (level != null) {
             options.setLosslessCompressionLevel(level);
         }
-        System.out.printf("Packing %s to %s%s...%n", sourceFile, packedFile,
-                level == null ? "" : " with level " + level);
-        byte[] unpacked = Files.readAllBytes(sourceFile);
-        long t1 = System.nanoTime();
-        byte[] packed = codec.compress(unpacked, options);
-        long t2 = System.nanoTime();
-        byte[] unpackedToCheck = codec.decompress(packed, options);
-        long t3 = System.nanoTime();
-        System.out.printf("%d bytes packed to %d bytes%n", unpacked.length, packed.length);
-        System.out.printf("Packing time: %.3f ms, %.3f MB/sec%n", (t2 - t1) * 1e-6,
-                unpacked.length / 1048576.0 / ((t2 - t1) * 1e-9));
-        System.out.printf("Unpacking time: %.3f ms, %.3f MB/sec%n", (t3 - t2) * 1e-6,
-                unpacked.length / 1048576.0 / ((t3 - t2) * 1e-9));
-        Files.write(packedFile, packed);
-        if (!Arrays.equals(unpacked, unpackedToCheck)) {
-            throw new AssertionError("Unpacking failed!");
-        } else {
-            System.out.println("OK");
+        for (int test = 1; test <= numberOfTests; test++) {
+            System.out.printf("Test %d/%d...%n", test, numberOfTests);
+            System.out.printf("Packing %s to %s%s...%n", sourceFile, packedFile,
+                    level == null ? "" : " with level " + level);
+            byte[] unpacked = Files.readAllBytes(sourceFile);
+            long t1 = System.nanoTime();
+            byte[] packed = codec.compress(unpacked, options);
+            long t2 = System.nanoTime();
+            byte[] unpackedToCheck = codec.decompress(packed, options);
+            long t3 = System.nanoTime();
+            System.out.printf("%d bytes packed to %d bytes%n", unpacked.length, packed.length);
+            System.out.printf("Packing time: %.3f ms, %.3f MB/sec%n", (t2 - t1) * 1e-6,
+                    unpacked.length / 1048576.0 / ((t2 - t1) * 1e-9));
+            System.out.printf("Unpacking time: %.3f ms, %.3f MB/sec%n", (t3 - t2) * 1e-6,
+                    unpacked.length / 1048576.0 / ((t3 - t2) * 1e-9));
+            Files.write(packedFile, packed);
+            if (!Arrays.equals(unpacked, unpackedToCheck)) {
+                throw new AssertionError("Unpacking failed!");
+            } else {
+                System.out.println("OK");
+            }
+            System.out.println();
         }
     }
 }
