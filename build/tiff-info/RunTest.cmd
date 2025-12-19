1273517895ecec5a8596e88b42f6cfc0bc556933
Daniel Alievsky <daniel.alievsky@gmail.com>
2025-12-19 16:58:15 +0200

    Moving RunTest to tiff-info folder

---
+++ build/tiff-info/RunTest.cmd
@@ -0,0 +1,12 @@
+@echo off
+if "%~1"=="" goto usage
+java -cp "%~dp0TiffInfoViewer.jar" %*
+goto end
+
+:usage
+echo Usage:
+echo   RunTest.cmd FullClassName [args...]
+echo Example:
+echo   RunTest.cmd net.algart.matrices.tiff.demo.io.TiffWriteSimpleDemo ../../../src/test/resources/demo/images/lenna.png lenna.tiff
+
+:end
---
+++ build/tiff-info/RunTest.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+if [ $# -eq 0 ]; then
+  echo "Usage:"
+  echo "  $0 FullClassName [args...]"
+  echo "Example:"
+  echo "  $0 net.algart.matrices.tiff.demo.io.TiffWriteSimpleDemo ../../../src/test/resources/demo/images/lenna.png lenna.tiff"
+  exit 1
+fi
+
+java -cp "$(dirname "$0")/TiffInfoViewer.jar" "$@"
